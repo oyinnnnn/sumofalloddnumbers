0f0e4a3d985d69d68444ac81f7b56050a0f6b93b
pc <pc@DESKTOP-P40ILKJ>
2022-09-06 09:45:06 +0100

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+
+            {
+                int i, limit = 100, sumOdd = 0;
+                Console.WriteLine("100:");
+
+                limit = Convert.ToInt32(Console.ReadLine());
+
+                for (i = 1; i <= 100; i += 2)
+                {
+                    sumOdd += i;
+                }
+
+                Console.WriteLine("Total sum: " + sumOdd);
+                Console.ReadLine();
+            }
+        }
+
+    }
+
+}
